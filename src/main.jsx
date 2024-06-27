20da775ccdce027413c625855d8e980cb4834cb7
Rokkan123 <daviddins@yahoo.com>
2024-06-27 16:34:54 -0400

    redirect file

--- src/main.jsx
+++ src/main.jsx
@@ -6,13 +6,11 @@ import { AuthProvider } from "./context/auth.context.jsx";
 import { StratagemProvider } from "./context/stratagem.context.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <AuthProvider>
-        <StratagemProvider>
-          <App />
-        </StratagemProvider>
-      </AuthProvider>
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <AuthProvider>
+      <StratagemProvider>
+        <App />
+      </StratagemProvider>
+    </AuthProvider>
+  </BrowserRouter>
 );
